058b1ea486f661790f73f1adb4bd15f8541fc010
carole tierney <caroleatierney@gmail.com>
2023-06-27 14:34:48 -0400

    Include quiz for week 4

--- Open Class/Week4/index.js
+++ Open Class/Week4/index.js
@@ -78,7 +78,137 @@
 
 // ============================================================================================
 
-let perfectSquare = param => Math.pow(param, 2);
-console.log(perfectSquare(3));
+// let perfectSquare = param => Math.pow(param, 2);
+// console.log(perfectSquare(3));
 
 // ============================================================================================
+// =================================== QUIZ for week 4// ======================================
+// ============================================================================================
+
+// const createFullName = (firstName, lastName) => firstName + " " + lastName;
+// console.log(createFullName("Ollie", "Smith")); //expected output: Ollie Smith
+
+
+// const doubleNumber = (number) => number * 2;
+// console.log(doubleNumber(10)); //expected output: 20
+
+
+
+// const getEvenNumbers = (array) => {
+//     let evenNumbers = [];
+//     for (let i of array) {
+//         if (i % 2 === 0) {
+//             evenNumbers.push(i);
+//         }
+//     }
+//     return evenNumbers;
+// }
+// console.log(getEvenNumbers([2, 3, 4, 5, 6, 7, 8, 9, 10])); // expected output: [2, 4, 6, 8, 10]
+
+
+
+// const customersx = [
+//     {
+//         name: 'Sam',
+//         address: {
+//             street: '1234 W Bell Rd',
+//             city: 'Phoenix',
+//             zip: '85308',
+//             state: 'AZ'
+//         },
+//         membershipLevel: 'GOLD',
+//         age: 32
+//     },
+// ];
+
+// let customer = customersx[0]
+// console.log(customer);
+
+// if (customer.name.charAt(0) === 'c') {
+//     console.log("yes");
+// } else {
+//     console.log("no");
+// }
+
+// if (customer.membershipLevel === 'SILVER') {
+//     console.log("YESSS");
+// } else {
+//     console.log("NOOOO");
+// }
+
+// if (customer.address.street !== undefined) {
+//     console.log("Street field has a value");
+// } else {
+//     console.log("NOOOO");
+// }
+
+// const customers = [
+//     {
+//         // Object 1: Name does not start with C, no match for city/state, not gold/platinum, not silver and under 29
+//         name: "Sam",
+//         address: {
+//             street: "1234 W Bell Rd",
+//             city: "Phoenix",
+//             zip: "85308",
+//             state: "AZ",
+//         },
+//         membershipLevel: "BRONZE",
+//         age: 32,
+//     },
+//     {
+//         // Object 2: Address field (street) is undefined, will not be added to the results array
+//         name: "Bob",
+//         address: {
+//             city: "Long Beach",
+//             zip: "9000",
+//             state: "CA",
+//         },
+//         membershipLevel: "GOLD",
+//         age: 32,
+//     },
+//     {
+//         // Object 4: Meets enough requirements, will be added to the results array
+//         name: "Christina",
+//         address: {
+//             street: "1234 Alamitos Ave",
+//             city: "Long Beach",
+//             zip: "90002",
+//             state: "CA",
+//         },
+//         membershipLevel: "SILVER",
+//         age: 21
+//     },
+// ];
+// const matchingCustomers = (custArray) => {
+//     let resultArray = [];
+//     for (let customer of custArray) {
+//         //checks if undefined address properties
+//         if (
+//             customer.address.street === undefined ||
+//             customer.address.city === undefined ||
+//             customer.address.zip === undefined ||
+//             customer.address.state === undefined
+//         ) {
+//             console.log("There are undefined address properties!", customer);
+//         } else if (
+//             //checks for uppercase C
+//             customer.name.charAt(0) === "C" ||
+//             //checks for City and State
+//             (customer.address.city === "Peoria" && customer.address.state === "AZ") ||
+//             //checks for membership level
+//             customer.membershipLevel === ("GOLD" || "PLATINUM") ||
+//             //checks for membership and age level
+//             (customer.membershipLevel === "SILVER" && customer.age < 29)
+//         ) {
+//             console.log("This customer fits the criteria", customer);
+//             resultArray.push(customer);
+//         } else {
+//             //else none of the above conditions are met
+//             console.log("This customer does not fit the criteria!", customer);
+//         }
+//     }
+//     console.log(resultArray);
+// };
+
+// matchingCustomers(customers);
+
