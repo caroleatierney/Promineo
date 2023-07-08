ed8d81fa84fb26ec975dc745e34ba4f68f3793d8
carole tierney <caroleatierney@gmail.com>
2023-07-07 22:14:08 -0400

    add open class for week 6

---
+++ Open Class/Week6/index_test.js
@@ -0,0 +1,23 @@
+// Unit test
+
+// Happy path
+
+// whenever we mention expect we mean chai.inspect
+var expect = chai.expect
+
+// describe takes a name for the unit test we are setting up and a function
+describe('MyFunctions', function() {
+    //  test do something function
+    describe('#doSomething', function() {
+        it('should concatenate the two parameters', function() {
+            var x = doSomething('Hello', 5);
+            expect(x).to.equal('Hello5');
+        });
+
+        it('should throw an error if the first parameter is not a string', function() {
+            expect(function() {
+                doSomething(5,5);
+            }).to.throw(Error);
+        });
+    });
+});
